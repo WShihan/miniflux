12db4340ef1ee48a66348886220c571412f67dda
WShihan <3443327820@qq.com>
2024-09-02 00:09:52 +0800

    fix: 创建订阅源错误sql

--- internal/storage/feed.go
+++ internal/storage/feed.go
@@ -240,7 +240,7 @@ func (s *Storage) CreateFeed(feed *model.Feed) error {
 			apprise_service_urls,
 			disable_http2,
 			description,
-			translatable,
+			translatable
 		)
 		VALUES
 			($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17, $18, $19, $20, $21, $22, $23, $24, $25, $26, $27)
